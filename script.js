565e860eec6164495b9c471cd9effbf4c35b779b
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-04-24 08:18:30 -0400

    Set up game board module, player factory, and win combinations

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
- 
+ // The game board 
+ const thegameBoard = (() => {
+     const board =["","", "", "", "", "","", "", "" ];
+
+return {    
+ board,
+ getBoard: () => board,
+ updateCell: (index, value) => { board[index] = value;},
+ reset: () => board.fill("")
+};
+})(); 
+
+// This is the factory function
+// It will create players
+const player = (name, mark) =>{
+    return {name, mark}
+}
+// Combination and game logic
+const playerX = player( "X");
+const playerY = player( "O");
+const Combination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+
+]
