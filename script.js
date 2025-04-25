5f73e437bfb58e396346c95acf3136adaca6edef
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-04-25 08:11:06 -0400

    Added game board module, player factory, move validation, and board logic

--- script.js
+++ script.js
@@ -2,13 +2,23 @@
  const thegameBoard = (() => {
      const board =["","", "", "", "", "","", "", "" ];
 
+const updateCell = (index, mark) => {
+    board[index] = mark;
+}
+
+const getBoard = () => board;
+
+const reset = () => {
+board.fill("");
+};
+
 return {    
  board,
  getBoard: () => board,
  updateCell: (index, value) => { board[index] = value;},
  reset: () => board.fill("")
 };
-})(); 
+})();  
 
 // This is the factory function
 // It will create players
@@ -18,6 +28,7 @@ const player = (name, mark) =>{
 // Combination and game logic
 const playerX = player( "X");
 const playerY = player( "O");
+// Wining Combinations
 const Combination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -29,3 +40,14 @@ const Combination = [
     [2, 4, 6]
 
 ]
+// Valid move checker parameter
+const yourMovecorrect = (tile) => {
+    if (title.innerText === "X"  || tile.innerText === "O"){
+        return false;
+    }
+    return true;
+};
+// Updates where players are on the board
+const boardUpdate = (index, mark) => {
+   thegameBoard.updateCell(index, mark);
+};
